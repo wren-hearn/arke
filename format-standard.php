79e54ba115e92ce134a60391e49971af02e4fba2
Jeffrey Hearn <jeffreyhearn@gmail.com>
2012-12-27 15:52:22 -0500

    Fixing error in image thumbnail display

--- format-standard.php
+++ format-standard.php
@@ -6,7 +6,7 @@ if ( has_post_thumbnail() )
 	<div class="post-thumbnail-wrapper">
 		<?php if( !is_single() ): ?><a href="<?php the_permalink(); ?>" class="thumbnail-link"><?php endif; ?>
 			<span class="post-thumbnail" style="background-image: url('<?php
-			$image_meta = wp_get_attachment_image_src( get_post_thumbnail_id(), 'big-thumbnail' )
+			$image_meta = wp_get_attachment_image_src( get_post_thumbnail_id(), 'big-thumbnail' );
 			echo $image_meta[0];
 			?>');">
 				<h1><?php the_title(); ?></h1>
