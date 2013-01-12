4b9e53ff74101281a64981326d741c1cf41f9721
Jeffrey Hearn <jeffreyhearn@gmail.com>
2013-01-12 14:42:39 -0500

    Menu work

--- functions.php
+++ functions.php
@@ -19,7 +19,7 @@ if( !is_admin() )
 }
 
 add_theme_support( 'automatic-feed-links' );
-add_theme_support( 'custom-background', array( 'default-image' => get_template_directory_uri() . '/img/wild_oliva.png' ) );
+add_theme_support( 'custom-background', array( 'default-image' => get_template_directory_uri() . '/img/orange-rings-bg.jpg' ) );
 
 add_theme_support( 'post-thumbnails' );
 set_post_thumbnail_size( 9999, 200, true );
@@ -31,13 +31,47 @@ add_image_size( 'big-thumbnail', 600, 200, true );
 add_theme_support( 'post-formats', array( 'aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat' ) );
 
 
-//register_nav_menu( 'top-menu', 'Top Menu' );
+// Register the main menu
+register_nav_menu( 'top-menu', 'Top Menu' );
+
+// Add a CSS class to parents of submenus
+add_filter( 'wp_nav_menu_objects', 'add_menu_parent_class' );
+function add_menu_parent_class( $items )
+{
+	$parents = array();
+	foreach ( $items as $item )
+	{
+		if ( $item->menu_item_parent && $item->menu_item_parent > 0 )
+		{
+			$parents[] = $item->menu_item_parent;
+		}
+	}
+	foreach ( $items as $item )
+	{
+		if ( in_array( $item->ID, $parents ) )
+		{
+			$item->classes[] = 'menu-parent-item'; 
+		}
+	}
+	return $items;    
+}
 
 
+/*
 // https://github.com/twittem/wp-bootstrap-navwalker
 // Register Custom Navigation Walker
 require_once('twitter_bootstrap_nav_walker.php');
-
-
-
+	<div class="navbar">
+		<div class="navbar-inner">
+				wp_nav_menu( array(
+					'menu'       => 'top_menu',
+					'depth'      => 2,
+					'container'  => false,
+					'menu_class' => 'nav',
+					//Process nav menu using our custom nav walker
+					'walker' => new twitter_bootstrap_nav_walker())
+				);
+		</div>  <!-- ./navbar-inner -->
+	</div>  <!-- ./navbar -->
+*/
 
--- header.php
+++ header.php
@@ -8,7 +8,9 @@
 		<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
 		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/font-awesome.css">
 
+		<link rel="stylesheet/less" type="text/css" href="<?php echo get_template_directory_uri(); ?>/less/bootstrap.less">
 		<link rel="stylesheet/less" type="text/css" href="<?php echo get_template_directory_uri(); ?>/less/sapphire.less">
+		
 		<script src="<?php echo get_template_directory_uri(); ?>/js/less-1.3.1.min.js" type="text/javascript"></script>
 
 		<?php wp_head(); ?>
@@ -24,28 +26,16 @@
 				<h2><?php bloginfo('name'); ?></h2>
 				<p class="muted"><?php bloginfo('description'); ?></p>
 
-<div class="navbar">
+				<?php
 
-	<div class="navbar-inner">
+				$args = array(
+					'theme_location' => 'top-menu',
+					'container' => false,
+					'menu_class' => 'top-navigation-menu'
+				);
 
-<?php
-    wp_nav_menu( array(
-        'menu'       => 'top_menu',
-        'depth'      => 2,
-        'container'  => false,
-        'menu_class' => 'nav',
-        //Process nav menu using our custom nav walker
-        'walker' => new twitter_bootstrap_nav_walker())
-    );
-?>
+				wp_nav_menu($args);
 
-
-
-
-	</div>  <!-- ./navbar-inner -->
-
-</div>  <!-- ./navbar -->
+				?>
 
 			</header>
-
-
