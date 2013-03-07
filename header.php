316cc47783854f50a74c39bdef7a35620c4f4a61
Jeffrey Hearn <cowbellemoo@gmail.com>
2013-03-07 03:00:59 -0500

    Adding CDN support

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
 		<title><?php is_home() ? bloginfo('name') : wp_title(''); ?></title>
 
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="">
+        <meta name="description" content="A WordPress theme based on Bootstrap and tuned for performance.">
 
 		<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/bootstrap-custom.min.css">
 
